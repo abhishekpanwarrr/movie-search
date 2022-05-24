4ef52a619324acdb37fc4d8b18d04c57af85f3a0
Abhishek Panwar <40629087+abhishekpanwarrr@users.noreply.github.com>
2022-05-24 13:55:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com/?apikey=b5a506c8";
+const API_URL = "https://www.omdbapi.com/?apikey=b5a506c8";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
